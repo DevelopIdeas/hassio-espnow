6e2550e57072e500a56c6b943bb749aa7e0632d0
Anton Priestley <anton@priestley.me>
2019-08-07 15:25:53 +0100

    Update index.js

--- espnow/src/index.js
+++ espnow/src/index.js
@@ -1,11 +1,16 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 
+console.log("Starting espnow2mqtt");
 let serialPath = '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0';
+console.log(`Using Port: ${serialPath}`);
+
 const port = new SerialPort(serialPath, {
     baudRate: 11520
 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+console.log("Adding Listener");
 parser.on('data', (data) => {
     console.log(data);
 });
